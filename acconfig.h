24f5d10f8db84c02cfcbbafdb2dea717868f781c
Greg A. Woods <woods@planix.ca>
1999-01-17 06:30:38 +0000

    - syslogd has a 'd' in it....

--- acconfig.h
+++ acconfig.h
@@ -1,4 +1,4 @@
-#ident	"@(#)newsyslog:$Name:  $:$Id: acconfig.h,v 1.2 1997/11/15 07:05:29 woods Exp $";
+#ident	"@(#)newsyslog:$Name:  $:$Id: acconfig.h,v 1.3 1999/01/17 06:30:38 woods Exp $";
 
 /*
  * Copyright (C) 1997 Planix, Inc. - All rights reserved.
@@ -16,7 +16,7 @@
 #define PATH_CONFIG		"/etc/newsyslog.conf"
 
 /* Default pid file */
-#define PATH_SYSLOG_PIDFILE	"/etc/syslog.pid"
+#define PATH_SYSLOGD_PIDFILE	"/etc/syslog.pid"
 
 /* File compression program */
 #define PATH_COMPRESS		"/usr/ucb/compress"
