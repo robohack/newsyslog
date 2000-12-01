902d74ee8142c51666934367c3d5d4d20857ecca
Greg A. Woods <woods@planix.ca>
2000-12-01 23:22:45 +0000

    - in parse_file() "prev" is only used as a character and because it's   only used in one side of an "if" statement it must be initialised.

--- newsyslog.c
+++ newsyslog.c
@@ -45,7 +45,7 @@
 static const char orig_rcsid[] =
 	"FreeBSD: newsyslog.c,v 1.14 1997/10/06 07:46:08 charnier Exp";
 static const char rcsid[] =
-	"@(#)newsyslog:$Name:  $:$Id: newsyslog.c,v 1.30 2000/12/01 20:39:50 woods Exp $";
+	"@(#)newsyslog:$Name:  $:$Id: newsyslog.c,v 1.31 2000/12/01 23:22:45 woods Exp $";
 #endif /* not lint */
 
 #ifdef HAVE_CONFIG_H
@@ -457,7 +457,7 @@ parse_file(files )
 	struct group   *grp;
 	int             eol;
 	int             lnum = 0;
-	int             prev;
+	char            prev = '\0';
 
 	if (strcmp(config_file, "-") == 0) {
 		fp = stdin;
