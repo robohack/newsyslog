3d7c3974e4e0505af45903b66fcd3229820b87db
Greg A. Woods <woods@planix.ca>
2003-07-08 16:56:55 +0000

    - silly hack for SunOS-4

--- signame.c
+++ signame.c
@@ -6,7 +6,7 @@
 #include <sys/cdefs.h>
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)newsyslog:$Name:  $:$Id: signame.c,v 1.6 2002/08/09 16:02:41 woods Exp $";*/
+static char sccsid[] = "@(#)newsyslog:$Name:  $:$Id: signame.c,v 1.7 2003/07/08 16:56:55 woods Exp $";*/
 #endif /* LIBC_SCCS and not lint */
 
 #ifdef HAVE_CONFIG_H
@@ -67,6 +67,40 @@ const char *const sys_signame[] = {
 	0		/* 32 : not defined */
 #  endif
 # endif	/* BSD */
+
+# ifdef UNIX_SUN_OS_4
+	"HUP",		/*  1 : SIGHUP */
+	"INT",		/*  2 : SIGINT */
+	"QUIT",		/*  3 : SIGQUIT */
+	"ILL",		/*  4 : SIGILL */
+	"TRAP",		/*  5 : SIGTRAP */
+	"ABRT",		/*  6 : SIGABRT & SIGIOT */
+	"EMT",		/*  7 : SIGEMT */
+	"FPE",		/*  8 : SIGFPE */
+	"KILL",		/*  9 : SIGKILL */
+	"BUS",		/* 10 : SIGBUS */
+	"SEGV",		/* 11 : SIGSEGV */
+	"SYS",		/* 12 : SIGSYS */
+	"PIPE",		/* 13 : SIGPIPE */
+	"ALRM",		/* 14 : SIGALRM */
+	"TERM",		/* 15 : SIGTERM */
+	"URG",		/* 16 : SIGURG */
+	"STOP",		/* 17 : SIGSTOP */
+	"TSTP",		/* 18 : SIGTSTP */
+	"CONT",		/* 19 : SIGCONT */
+	"CHLD",		/* 20 : SIGCHLD */
+	"TTIN",		/* 21 : SIGTTIN */
+	"TTOU",		/* 22 : SIGTTOU */
+	"IO",		/* 23 : SIGIO */
+	"XCPU",		/* 24 : SIGXCPU */
+	"XFSZ",		/* 25 : SIGXFSZ */
+	"VTALRM",	/* 26 : SIGVTALRM */
+	"PROF",		/* 27 : SIGPROF */
+	"WINCH",	/* 28 : SIGWINCH */
+	"LOST",		/* 29 : SIGLOST */
+	"USR1",		/* 30 : SIGUSR1 */
+	"USR2",		/* 31 : SIGUSR2 */
+# endif	/* UNIX_SUN_OS_4 */
 
 # ifdef __linux__
 	/*
@@ -173,7 +207,7 @@ const char *const sys_signame[] = {
 # endif
 
 # if !(defined(BSD) || defined(__linux__) || defined(__SVR4))
-#  include "ERROR: your system has no native sys_signame is not yet supported with a custom one"
+#  include "ERROR: your system has no native sys_signame and is not yet supported with a custom one"
 # endif
 
 #endif /* ! SYS_SIGNAME_DECLARED && ! HAVE_STR2SIG && ! HAVE_SIG2STR */
