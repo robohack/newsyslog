a299065d50b976495d2c4a7ebb3ff57aa7a8c897
Greg A. Woods <woods@planix.ca>
2009-04-26 03:50:11 +0000

    - new replacement stub functions for ancient and broken systems.

---
+++ setgroupent.c
@@ -0,0 +1,15 @@
+#ident	"@(#)newsyslog:$Name:  $:$Id: setgroupent.c,v 1.1 2009/04/26 03:50:11 woods Exp $"
+
+/*
+ * setgroupent() -- a replacement stub
+ *
+ * Copyright assigned to the Public Domain.
+ */
+
+/* just a stub -- it does nothing */
+int
+setgroupent(stayopen)
+	int stayopen;
+{
+	return 1;
+}
---
+++ setpassent.c
@@ -0,0 +1,15 @@
+#ident	"@(#)newsyslog:$Name:  $:$Id: setpassent.c,v 1.1 2009/04/26 03:50:11 woods Exp $"
+
+/*
+ * setpassent() -- a replacement stub
+ *
+ * Copyright assigned to the Public Domain.
+ */
+
+/* just a stub -- it does nothing */
+int
+setpassent(stayopen)
+	int stayopen;
+{
+	return 1;
+}
