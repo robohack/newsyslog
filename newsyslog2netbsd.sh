e33998df9d5fd756ab7683d89278ca2b3c403315
Greg A. Woods <woods@planix.ca>
2020-05-22 17:52:16 -0700

    improve netsyslog2netbsd.sh
    - use "git describe --tags" to get release ID
    - run autoreconf first
    - make a separate "build" subdirectory for configuring

--- newsyslog2netbsd.sh
+++ newsyslog2netbsd.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 #
 #	newsyslog2netbsd - upgrade from latest newsyslog release
 #
@@ -18,15 +18,22 @@ LOCALSTATEDIR="/var"
 NEWSYSLOG_CONF=${SYSCONFDIR}/newsyslog.conf
 SYSLOGD_PID=${LOCALSTATEDIR}/run/syslogd.pid
 
-IMPORTDIR="import.d"
-
 #
 # NOTHING user adjustable below this line!
 #
 
-PWD=$(/bin/pwd)
-version=$(basename $PWD | sed -e 's/newsyslog-//')
-releasetag=$(basename $PWD | sed -e 's/\./_/g')
+IMPORTDIR="import.d"
+BUILDDIR="nbsd-import"
+
+version=$(git describe --tags)
+releasetag=$(echo $version | sed -e 's/\./_/g')
+
+set -e
+
+autoreconf -f -i
+
+mkdir ${BUILDDIR}
+cd ${BUILDDIR}
 
 cpsed ()
 {
@@ -52,27 +59,29 @@ mkdir ${IMPORTDIR}
 if [ -f Makefile ] ; then
 	make distclean
 fi
-AWK=awk ./configure --prefix=${PREFIX} --bindir=${BINDIR} --sysconfdir=${SYSCONFDIR} --localstatedir=${LOCALSTATEDIR} --with-newsyslog-conf=${NEWSYSLOG_CONF} --with-syslogd_pid=${SYSLOGD_PID} --with-gzip
+AWK=awk ../configure --prefix=${PREFIX} --bindir=${BINDIR} --sysconfdir=${SYSCONFDIR} --localstatedir=${LOCALSTATEDIR} --with-newsyslog-conf=${NEWSYSLOG_CONF} --with-syslogd_pid=${SYSLOGD_PID} --with-gzip
 
 # note the couple of renames....
-cpsed AUTHORS ${IMPORTDIR}/AUTHORS
-cpsed COPYING ${IMPORTDIR}/COPYING
+cpsed ../AUTHORS ${IMPORTDIR}/AUTHORS
+cpsed ../COPYING ${IMPORTDIR}/COPYING
 cpsed Makefile.BSD ${IMPORTDIR}/Makefile
-cpsed NEWS ${IMPORTDIR}/NEWS
-cpsed README ${IMPORTDIR}/README
-cpsed ToDo ${IMPORTDIR}/ToDo
-cpsed VERSION ${IMPORTDIR}/VERSION
+cpsed ../NEWS ${IMPORTDIR}/NEWS
+cpsed ../README ${IMPORTDIR}/README
+cpsed ../ToDo ${IMPORTDIR}/ToDo
+cpsed ../VERSION ${IMPORTDIR}/VERSION
 cpsed newsyslog.8so ${IMPORTDIR}/newsyslog.8
 cpsed newsyslog.conf.5so ${IMPORTDIR}/newsyslog.conf.5so
-cpsed newsyslog.c ${IMPORTDIR}/newsyslog.c
-cpsed newsyslog.h ${IMPORTDIR}/newsyslog.h
-cpsed sig2str.c ${IMPORTDIR}/sig2str.c
-cpsed str2sig.c ${IMPORTDIR}/str2sig.c
-cpsed newsyslog.conf ${IMPORTDIR}/newsyslog.conf
-cpsed newsyslog2netbsd.sh ${IMPORTDIR}/newsyslog2netbsd.sh
+cpsed ../newsyslog.c ${IMPORTDIR}/newsyslog.c
+cpsed ../newsyslog.h ${IMPORTDIR}/newsyslog.h
+cpsed ../sig2str.c ${IMPORTDIR}/sig2str.c
+cpsed ../str2sig.c ${IMPORTDIR}/str2sig.c
+cpsed ../newsyslog.conf ${IMPORTDIR}/newsyslog.conf
+cpsed ../newsyslog2netbsd.sh ${IMPORTDIR}/newsyslog2netbsd.sh
 
 # tell them what to do....
 echo "# Now do this:"
-echo "cd ${IMPORTDIR} ; cvs import -m 'Import of Planix newsyslog version $version' src/usr.bin/newsyslog PLANIX $releasetag"
+echo "cd ${BUILDDIR}/${IMPORTDIR} && cp * /usr/src/usr.bin/newsyslog"
+# XXX if NetBSD is/was still using CVS:
+#echo "cd ${BUILDDIR}/${IMPORTDIR} ; cvs import -m 'Import of Planix newsyslog version $version' src/usr.bin/newsyslog PLANIX $releasetag"
 
 exit 0
